d0a70a3e57943357eec64fe57733d4f68cba14d3
iskanye <iskanyeban@gmail.com>
2025-10-31 10:50:34 +0300

    feat: billing client

---
+++ internal/grpc/billing/client.go
@@ -0,0 +1,50 @@
+package billings
+
+import (
+	"context"
+	"net"
+	"strconv"
+
+	"github.com/iskanye/utilities-payment-proto/billing"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+type clientApi struct {
+	billing billing.BillingClient
+}
+
+type Billing interface {
+	PayBill(
+		ctx context.Context,
+		billID int64,
+	) error
+}
+
+func New(
+	host string,
+	port int,
+) (clientApi, error) {
+	cc, err := grpc.NewClient(
+		net.JoinHostPort(host, strconv.Itoa(port)),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return clientApi{}, status.Error(codes.Unavailable, err.Error())
+	}
+
+	return clientApi{billing.NewBillingClient(cc)}, nil
+}
+
+func (c *clientApi) PayBill(
+	ctx context.Context,
+	billID int64,
+) error {
+	_, err := c.billing.PayBill(ctx, &billing.PayRequest{BillId: billID})
+	if err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	return nil
+}
